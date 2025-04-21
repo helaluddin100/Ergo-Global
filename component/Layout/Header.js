2d6b89fa46c0904e066cb93fd150fea12ab6a6a3
wid100 <helal.womenindigital@gmail.com>
2025-04-21 16:24:12 +0600

    remove image

--- component/Layout/Header.js
+++ component/Layout/Header.js
@@ -6,7 +6,31 @@ function Header() {
 
   return (
     <>
-      <h2>Header</h2>
+      <nav className="navbar">
+        <div className="container">
+          <header className="header">
+            <div className="logo">
+              <Link href="/">
+                <img src="/img/logo/logo.png" alt="Logo" />
+              </Link>
+            </div>
+            <ul className="nav-links">
+              <li>
+                <Link href="/">Contact</Link>
+              </li>
+              <li>
+                <Link href="/">How it works</Link>
+              </li>
+              <li>
+                <Link href="/" className="contact-btn">Get Started</Link>
+              </li>
+              <li>
+                <Link href="/contact">Contact</Link>
+              </li>
+            </ul>
+          </header>
+        </div>
+      </nav>
     </>
   );
 }
